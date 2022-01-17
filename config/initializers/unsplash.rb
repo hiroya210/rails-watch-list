1f0477c2b2e2df21ff04da3768782ed3a181bfb3
Hiroya Takemura <hirotakemura210@gmail.com>
2022-01-17 10:07:55 +0900

    removed unsplash

--- config/initializers/unsplash.rb
+++
@@ -1,6 +0,0 @@
-Unsplash.configure do |config|
-    config.application_access_key = "ZpnJ8mP1lYZXnFkKXPms7EXCJHPNkvdrWMoFqBA-n_s"
-    config.application_secret = "BSetGUlfz7-c7aVK65d3UligfGtZ4nigQE6Uvu8LtyA"
-    config.application_redirect_uri = "https://your-application.com/oauth/callback"
-    config.utm_source = "alices_terrific_client_app"
-  end
