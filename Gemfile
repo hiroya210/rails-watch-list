f7df79c628d25fe0e1b570ae05f6789b2ea2a874
Hiroya Takemura <hirotakemura210@gmail.com>
2022-01-19 02:37:54 +0900

    Improved Seeds

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'unsplash'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require "json"
 require "open-uri"
+require "faker"
 
+Bookmark.destroy_all
 Movie.destroy_all
 Review.destroy_all
 List.destroy_all
@@ -8,8 +10,8 @@ List.destroy_all
 puts "Creating Movies"
 num = 0
 5.times do
-url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["API_KEY"]}&language=en-US&page=#{num += 1}"
-movies = JSON.parse(URI.open(url).read)
+  url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV["API_KEY"]}&language=en-US&page=#{num += 1}"
+  movies = JSON.parse(URI.open(url).read)
 
   movies["results"].each do |result|
     Movie.create(
@@ -23,6 +25,8 @@ movies = JSON.parse(URI.open(url).read)
 end
 puts "Done!"
 
+#Lists
+
 puts "Creating Lists"
 file1 = URI.open('https://res.cloudinary.com/yahiro/image/upload/v1641892089/rails%20watchlist/toprated.jpg')
 list1 = List.new( name: "Top Rated")
@@ -31,27 +35,51 @@ list1.save
 puts "list 1 done!"
 
 file2 = URI.open("https://res.cloudinary.com/yahiro/image/upload/v1641892089/rails%20watchlist/superhero.png")
-list2 = List.new( name: "SuperHeroes")
+list2 = List.new( name: "Everyone's Favorite")
 list2.photo.attach(io: file2, filename: 'superhero.png', content_type: 'image/png')
 list2.save
 puts "list 2 done!"
 
 file3 = URI.open("https://res.cloudinary.com/yahiro/image/upload/v1641892089/rails%20watchlist/drama.jpg")
-list3 = List.new( name: "Drama")
+list3 = List.new( name: "My Recomendation")
 list3.photo.attach(io: file3, filename: 'drama.jpg', content_type: 'image/jpg')
 list3.save
 puts "list 3 done!"
 
 
 file4 = URI.open("https://res.cloudinary.com/yahiro/image/upload/v1641892089/rails%20watchlist/classic.jpg")
-list4 = List.new( name: "classic")
+list4 = List.new( name: "Old but Gold")
 list4.photo.attach(io: file4, filename: 'classic.jpg', content_type: 'image/jpg')
 list4.save
 puts "list 4 done!"
 
 
 file5 = URI.open("https://res.cloudinary.com/yahiro/image/upload/v1641892088/rails%20watchlist/asian.jpg")
-list5 = List.new( name: "asian")
+list5 = List.new( name: "Random")
 list5.photo.attach(io: file5, filename: 'asian.jpg', content_type: 'image/jpg')
 list5.save
 puts "list 5 done!"
+
+
+#Bookmarks
+
+puts "Creating bookmarks"
+movie_id = 1
+lists = [list1, list2, list3, list4, list5]
+
+lists.each do |list|
+  10.times do
+    Bookmark.create( movie_id: Movie.all.sample.id, list_id: list.id, comment: Faker::GreekPhilosophers.quote )
+  end
+end
+
+puts "Bookmarks done!"
+
+
+
+
+
+
+
+
+
