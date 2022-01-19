1bd1e19121e0935a6f7565d228ef0ea6512fc6b6
Hiroya Takemura <hirotakemura210@gmail.com>
2022-01-19 12:05:39 +0900

    responsive to mac

--- app/javascript/components/splide.js
+++ app/javascript/components/splide.js
@@ -1,6 +1,18 @@
 import Splide from '@splidejs/splide';
 
+
+
 export const slides = () => {
+
+  let maxHeight = window.innerHeight;
+  if ((maxHeight < 1230) && (maxHeight > 1400)){
+    let maxHeight = "100vh"
+  }
+  else{
+    let maxHeight = "60vh"
+  }
+
+
   const thumbnails = new Splide( '.splide', {
         trimSpace   : false,
         focus       : 'center',
@@ -20,7 +32,8 @@ export const slides = () => {
     }).mount();
 
   const main = new Splide( '#main-slider', {
-        fixedHeight: "100vh",
+        fixedHeight: maxHeight,
+        fixedWidth: "95vw",
         type      : 'fade',
         rewind    : true,
         pagination: false,
@@ -29,7 +42,7 @@ export const slides = () => {
           560: {
             fixedWidth : "100",
             fixedHeight: "50vh",
-          },
+          }
         },
   }).mount();
 
